4ac8a1ebb95068731b2fcd46e341353c5ca08482
AntonAcorn <ShustikovAnimation@gmail.com>
2022-11-20 15:10:49 +0300

    First hibernateApp

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    public static void main(String[] args) {   //Явно здесь файл hibernate.properties не подключается, но под копотом подключается. поэтому файл обязан называться hibernate.properties
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class); //передаем класс, который помечен @Entity
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();  //Создаем фабрику
+        Session session = sessionFactory.getCurrentSession();    //Из фабрики создаем сессиию
+        try {
+            session.beginTransaction();  //Открываем транзакцию
+            Person person = session.get(Person.class, 1);  //1. Указываем какую именно сущность хотим получить (т.е. через Person.class он обращается к нужной таблице)
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit(); //выполняем транзацию
+        } finally {
+            sessionFactory.close(); //закрываем фабрику
+        }
     }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,2 +1,51 @@
-package org.example.model;public class Person {
+package org.example.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity //сущность для БД
+@Table(name = "Person") //Название таблицы из БД. Среда помечает ошибку, но она не видит, базу. Все работает
+public class Person {
+    @Id
+    @Column(name = "id")// пишем название столбца из самой таблицы, чтобы БД сопоставлялась с классом
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private String age;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
